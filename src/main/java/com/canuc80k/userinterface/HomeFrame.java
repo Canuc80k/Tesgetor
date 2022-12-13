0ac4537acd8dd03923994dc146fa16d613df89bd
Canuc80k <Canuc80k@gmail.com>
2022-12-14 01:10:46 +0700

    Basic UI

--- src/main/java/com/canuc80k/userinterface/HomeFrame.java
+++ src/main/java/com/canuc80k/userinterface/HomeFrame.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JProgressBar;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JTextPane;
+import java.awt.CardLayout;
 
 public class HomeFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -14,7 +19,7 @@ public class HomeFrame extends JFrame {
 
 	public HomeFrame() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setSize(600, 600);
+		setSize(600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -23,6 +28,36 @@ public class HomeFrame extends JFrame {
 		JLabel topLabel = new JLabel("Dont_generatetest_me_pls");
         topLabel.setFont(new Font("Open Sans ExtraBold", Font.PLAIN, 24));
 		contentPane.add(topLabel, BorderLayout.NORTH);
+		
+		JProgressBar progressBar = new JProgressBar();
+		contentPane.add(progressBar, BorderLayout.SOUTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton, BorderLayout.WEST);
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.SOUTH);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panel_2.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panel_2.add(btnNewButton_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3, BorderLayout.CENTER);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_4 = new JPanel();
+		panel_3.add(panel_4, BorderLayout.SOUTH);
+		panel_4.setLayout(new CardLayout(0, 0));
 	}
 
 }
