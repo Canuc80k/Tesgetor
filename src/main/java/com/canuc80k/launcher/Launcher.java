2bc0f9f71ee5a9dc28fcaa766b755e82f12d77f9
Canuc80k <Canuc80k@gmail.com>
2022-12-15 11:35:07 +0700

    remove guide

--- src/main/java/com/canuc80k/launcher/Launcher.java
+++ src/main/java/com/canuc80k/launcher/Launcher.java
@@ -3,6 +3,9 @@ package com.canuc80k.launcher;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.canuc80k.compiler.CPPCompiler;
 import com.canuc80k.font.OpenSansFontInitor;
 import com.canuc80k.userinterface.HomeFrame;
@@ -17,9 +20,8 @@ public class Launcher {
 
             CPPCompiler a = new CPPCompiler();
             a.compile_gplusplus(
-                new File("C:\\Users\\Lenovo\\Desktop\\a.cpp"),
-                new File("C:\\Users\\Lenovo\\Desktop\\b\\a.exe")
-            );
+                    new File("C:\\Users\\Lenovo\\Desktop\\a.cpp"),
+                    new File("C:\\Users\\Lenovo\\Desktop\\b\\a.exe"));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
--- src/main/java/com/canuc80k/userinterface/HomeFrame.java
+++ src/main/java/com/canuc80k/userinterface/HomeFrame.java
@@ -1,63 +1,67 @@
 package com.canuc80k.userinterface;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JProgressBar;
-import javax.swing.JButton;
-import java.awt.FlowLayout;
-import javax.swing.JTextPane;
-import java.awt.CardLayout;
 
 public class HomeFrame extends JFrame {
+    private final URL GUIDE_GIF_URL = HomeFrame.class.getResource("/guide/guide.gif");
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	
+    public ImageIcon scaleImage(ImageIcon icon, int w, int h)
+    {
+        int nw = icon.getIconWidth();
+        int nh = icon.getIconHeight();
 
-	public HomeFrame() {
+        if(icon.getIconWidth() > w)
+        {
+          nw = w;
+          nh = (nw * icon.getIconHeight()) / icon.getIconWidth();
+        }
+
+        if(nh > h)
+        {
+          nh = h;
+          nw = (icon.getIconWidth() * nh) / icon.getIconHeight();
+        }
+
+        return new ImageIcon(icon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
+    }
+
+    public HomeFrame() {        
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setSize(600, 400);
+		setSize(400, 600);
+		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.decode("#6487db"));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		
-		JLabel topLabel = new JLabel("Dont_generatetest_me_pls");
-        topLabel.setFont(new Font("Open Sans ExtraBold", Font.PLAIN, 24));
-		contentPane.add(topLabel, BorderLayout.NORTH);
-		
-		JProgressBar progressBar = new JProgressBar();
-		contentPane.add(progressBar, BorderLayout.SOUTH);
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		JButton btnNewButton = new JButton("New button");
-		panel.add(btnNewButton, BorderLayout.WEST);
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1, BorderLayout.NORTH);
-		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2, BorderLayout.SOUTH);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		panel_2.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		panel_2.add(btnNewButton_2);
-		
-		JPanel panel_3 = new JPanel();
-		panel.add(panel_3, BorderLayout.CENTER);
-		panel_3.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_4 = new JPanel();
-		panel_3.add(panel_4, BorderLayout.SOUTH);
-		panel_4.setLayout(new CardLayout(0, 0));
+		JPanel topPanel = new JPanel();
+		contentPane.add(topPanel, BorderLayout.CENTER);
+		topPanel.setLayout(new BorderLayout(0, 0));
+
+        JPanel bottomPanel = new JPanel();
+		contentPane.add(bottomPanel, BorderLayout.CENTER);
+		bottomPanel.setLayout(new BorderLayout(0, 0));
+	
+		JButton controller = new JButton("New button");
+		controller.setPreferredSize(new Dimension(Integer.MIN_VALUE, 500));
+		topPanel.add(controller, BorderLayout.NORTH);
 	}
 
 }
