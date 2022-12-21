7e1e05be77d52e74927fec4251f188c627a50eb6
Canuc80k <Canuc80k@gmail.com>
2022-12-21 20:52:43 +0700

    Fix log(0) error

--- src/main/java/com/canuc80k/constant/TestcaseFileNameType.java
+++ src/main/java/com/canuc80k/constant/TestcaseFileNameType.java
@@ -6,11 +6,12 @@ public enum TestcaseFileNameType {
 
     public static String getFileName(TestcaseFileNameType type, int currentTestcaseIndex, int lastTestcaseFileNameLength) {
         if (type == TestcaseFileNameType.NORMAL) return String.valueOf(currentTestcaseIndex);
-        if (lastTestcaseFileNameLength == 0) return "0"; 
 
         String filename = "";
-        int currentLength = (int)(Math.log10(currentTestcaseIndex) + 1);
+        int currentLength = 1;
+        if (currentTestcaseIndex != 0) currentLength = (int)(Math.log10(currentTestcaseIndex) + 1);
         int amount0prefix = lastTestcaseFileNameLength - currentLength;
+        
         for (int i = 0; i < amount0prefix; i ++) filename += "0";
         filename += String.valueOf(currentTestcaseIndex);
 
